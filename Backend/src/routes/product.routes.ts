290339cefad80413a1972f359cd9f45c8327e32a
Sourabh Kumar Mahuvar <sourabhmahuvar@gmail.com>
2026-01-02 13:23:57 +0530

    fix: resolve category filtering, sorting, and api route issues" -m "1. Backend (Controller): Fixed Category filtering logic (added regex support for case-insensitive name lookup) and handled 'Category Not Found' gracefully. 2. Frontend (Category): Fixed bug where subcategory filter did not reset when changing main categories. 3. Backend (Controller): Fixed sorting logic (applied sort before pagination) and fixed salePrice calculation during updates. 4. Backend (Routes): Reordered product routes to prevent CastError on 'repair-prices' endpoint. 5. Feat: Added repair-prices utility to fix mixed price data.

--- Backend/src/routes/product.routes.ts
+++ Backend/src/routes/product.routes.ts
@@ -1,37 +1,42 @@
-import { Router } from 'express';
-import { createProductHandler, getProductsHandler, getProductByIdHandler ,deleteProductHandler,updateProductHandler} from '../controllers/product.controller';
-import { protect, adminOnly } from '../middlewares/auth.middleware';
-import { validate } from '../middlewares/zodValidation';
-import { productSchema } from '../utils/zodSchemas';
+import { Router } from "express";
+import {
+  createProductHandler,
+  getProductsHandler,
+  getProductByIdHandler,
+  deleteProductHandler,
+  updateProductHandler,
+  repairProductPricesHandler,
+} from "../controllers/product.controller";
+import { protect, adminOnly } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/zodValidation";
+import { productSchema } from "../utils/zodSchemas";
 
 const router = Router();
 
 // Public Routes
-router.get('/', getProductsHandler);       // GET /api/v1/products
-router.get('/:id', getProductByIdHandler); // GET /api/v1/products/:id
+router.get("/repair-prices", repairProductPricesHandler); //GET /api/v1/repair-prices
+router.get("/", getProductsHandler); // GET /api/v1/products
+router.get("/:id", getProductByIdHandler); // GET /api/v1/products/:id
 
 // Protected Admin Routes
 // Uses: Auth Check -> Admin Check -> Zod Validation -> Controller
 router.post(
-  '/', 
-  protect, 
-  adminOnly, 
-  validate(productSchema), 
+  "/",
+  protect,
+  adminOnly,
+  validate(productSchema),
   createProductHandler
 );
 
-router.delete(
-  '/:id', 
-  protect, 
-  adminOnly, 
-  deleteProductHandler
-);
+router.delete("/:id", protect, adminOnly, deleteProductHandler);
 
 router.put(
-  '/:id', 
-  protect, 
-  adminOnly, 
+  "/:id",
+  protect,
+  adminOnly,
   // validate(productSchema), if validation is strict then we can remove it or use it
   updateProductHandler
 );
+
+
 export default router;
